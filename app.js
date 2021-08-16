c36943079c797d236855776ec8abef0dfc1cef11
canberko560 <canberkvarli@gmail.com>
2021-08-16 13:39:32 -0700

    fix app.js

--- app.js
+++ app.js
@@ -9,9 +9,13 @@ const passport = require('passport');
 app.get('/', (req, res) => res.send("Forest Library test"));
 
 const db = require('./config/keys').mongoURI;
+
+
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('connected to MongoDB successfully'))
     .catch(err => console.log(err));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 
@@ -19,8 +23,6 @@ app.listen(port, () => console.log(`server is running on port ${port}`));
 app.use(passport.initialize());
 require('./config/passport')(passport);
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.use("/api/users", users);
 
---
+++ validation/review.js
@@ -0,0 +1,21 @@
+const Validator = require("validator");
+const validText = require("./valid-text");
+
+module.exports = function validateReviewInput(data) {
+    let errors = {};
+
+    data.review = validText(data.review) ? data.review : ''
+
+     if(!Validator.isEmpty(data.review)){
+        errors.review = "Review is required"
+    }
+
+    if(!Validator.isEmpty(data.password)){
+        errors.password = "Password is required"
+    }
+
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0
+    }
+}
