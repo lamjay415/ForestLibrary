52555cfc88a3646016c3ccdd885e147a44bbc606
hilalbbalci <drhilalbiten@gmail.com>
2021-08-16 14:01:11 -0700

    login route has done

--- routes/api/users.js
+++ routes/api/users.js
@@ -35,4 +35,25 @@ router.post('/register', (req, res) => {
     });
 });
 
+router.post("/login", (req, res)=> {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({username})
+    .then(user => {
+        if(!user) {
+            return res.status(404).json({username: "This user doesn't not exist"});
+        }
+        bcrypt.compare(password, user.password)
+            .then(isMatch=> {
+                if(isMatch) {
+                    res.json({msg: "Success!"});
+                } else {
+                    return res.status(400).json({password: "Incorrect password"})
+                }
+                
+            })
+    });
+})
+
 module.exports = router;
