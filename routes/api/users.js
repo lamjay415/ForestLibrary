c7875c2e52fc8ac6a16da0d504d66d393469062e
hilalbbalci <drhilalbiten@gmail.com>
2021-08-16 14:56:41 -0700

    user jwt has done

--- routes/api/users.js
+++ routes/api/users.js
@@ -47,7 +47,23 @@ router.post("/login", (req, res)=> {
         bcrypt.compare(password, user.password)
             .then(isMatch=> {
                 if(isMatch) {
-                    res.json({msg: "Success!"});
+                    const payload ={
+                        id: user.id,
+                        username: user.username,
+                    }
+                    jwt.sign(
+                        payload,
+                        keys.secretOrKey,
+                        {expiresIn: 3600},
+                        (err, token)=> {
+                            res.json({ 
+                                success: true,
+                                token: "Bearer " + token
+                             });
+                        }
+
+                    )
+                    
                 } else {
                     return res.status(400).json({password: "Incorrect password"})
                 }
