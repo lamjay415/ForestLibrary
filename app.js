dcf9793e47faf91e55a2e2171a5dd11dce8b06fb
hilalbbalci <drhilalbiten@gmail.com>
2021-08-16 10:56:04 -0700

    done users route

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-
+const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
 
 app.get('/', (req, res) => res.send("Forest Library test"));
 
-const db = require('./config/keys').mongoURI;
+
 mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('connected to MongoDB successfully'))
     .catch(err => console.log(err));
 
+app.use("/api/users", users);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
