087d7ffc9043d74e3d577a936c4cdc383894571b
canberko560 <canberkvarli@gmail.com>
2021-08-17 11:56:42 -0700

    add stuff

--- routes/api/tree.js
+++ routes/api/tree.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const router = express.Router();
-const tree = require('../../models/Tree')
+const User = require("../../models/user");   
+const Tree = require('../../models/Tree');
 
 const passport = require('passport');
 
 
 router.get("/", (req, res) => {
-   res.json({ msg: "This is the tree route" }); 
+   res.json({ msg: "This is the landing page." }); 
 });
+
+router.post("/tree", (req, res) => {
+   //if there is a user which there is a tree.
+   User.findOne({username: req.body.username})
+   .then(user => {
+      if(user){
+         return res.status(400).json({username: "There is one tree registered already"})
+      }else{
+         const newTree = new Tree({
+            _id: Schema.Types.ObjectId,
+            leaves: [{ type: Schema.Types.ObjectId, ref: 'Leaf' }]
+         });
+         newTree.save()
+         .then(tree => res.json(tree))
+         .catch(err => console.log(err))
+      }
+   })
+})
+
 
 module.exports = router;
