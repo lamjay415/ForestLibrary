011cbfb3f641b7bfe9f7756029f331a5195b474b
hilalbbalci <drhilalbiten@gmail.com>
2021-08-16 17:17:21 -0700

    get request for tree done

--- models/tree.js
+++ models/tree.js
@@ -2,10 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TreeSchema = new Schema({
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
     leaves: [{ type: Schema.Types.ObjectId, ref: 'Leaf' }],
     branches: {
         type: String
+    },
+    date: {
+      type: Date,
+      default: Date.now
     }
-  })
+  });
 
 module.exports = Tree = mongoose.model("Tree", TreeSchema);
