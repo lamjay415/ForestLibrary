3dc03576adaba0ca58829ae38832a8b0abe8733d
Jay Lam <lamjay415@gmail.com>
2021-08-16 17:42:16 -0700

    completed frontend auth

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Forest Library</h1>
+        <footer>
+          Project created by John, Hilal, Jay
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
