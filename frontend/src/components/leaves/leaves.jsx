1387d81bcf8f9e9bf1ac93c051dc3acffde623ec
hilalbbalci <drhilalbiten@gmail.com>
2021-08-17 23:52:17 -0700

    worked on leaf backend and fronted

--- frontend/src/components/leaves/leaves.jsx
+++ frontend/src/components/leaves/leaves.jsx
@@ -16,26 +16,26 @@ class Leaf extends React.Component {
         this.props.fetchLeaves();
     }
 
-    // componentWillReceiveProps(newState) {
-    //     this.setState({ leaves: newState.leaves });
-    // }
+    componentWillReceiveProps(newState) {
+        this.setState({ leaves: newState.leaves });
+    }
 
     render() {
         
         if(!this.state.leaves) return null;
-        // if (this.state.leaves.length === 0) {
-        //     return (
-        //         <div>
-        //             <h3>There are no leaves</h3>
-        //             <Link to="/leaves/new_leaf">Add a Leaf</Link>
-        //         </div>)
-        // } 
+        if (this.state.leaves.length === 0) {
+            return (
+                <div>
+                    <h3>There are no leaves</h3>
+                    <Link to="/leaves/new_leaf">Add a Leaf</Link>
+                </div>)
+        } 
         console.log(this.state.leaves);
             return (
                 <div>
                     <h2>All leaves</h2>
-                    {this.state.leaves.map(leaf => (
-                        <li> {leaf.title} </li>
+                    {this.state.leaves.map((leaf, idx) => (
+                        <li key={idx}> <LeafBox title={leaf.title}/> </li>
                     ))}
                 </div>
             );
