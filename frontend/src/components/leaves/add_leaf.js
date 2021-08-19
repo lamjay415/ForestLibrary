b0ea2afb3deafac268297b164affd5c077d8b0f0
hilalbbalci <drhilalbiten@gmail.com>
2021-08-19 14:20:59 -0700

    search book, get data, add as a new book have done

--- frontend/src/components/leaves/add_leaf.js
+++ frontend/src/components/leaves/add_leaf.js
@@ -4,7 +4,7 @@ import LeafBox from './leaf_box';
 class AddLeaf extends React.Component {
     constructor(props) {
         super(props);
-        this.state = this.props.newLeaf;
+        // this.state = this.props.newLeaf;
        
 
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -19,28 +19,38 @@ class AddLeaf extends React.Component {
     handleSubmit(e) {
         e.preventDefault();
         let leaf = {
-            title: this.state.title,
-            userId: this.state.userId
-            
+            title: this.props.data[0].volumeInfo.title,
+            userId: this.props.currentUser,
+            category: this.props.data[0].volumeInfo.categories[0],    
         };
-
+        console.log(leaf);
         this.props.addLeaf(leaf);
         this.setState({ title: '' });
     }
 
     update(field) {
-        return e => this.setState({ [field]: e.currentTarget.value });
-    }
+        return (e => {
+            this.setState({ [field]: e.currentTarget.value });
+            
+    })}
 
     render() {
+        if(!this.props.data) return null;
+        if(this.props.data.length === 0) return null;
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <div>
                         <input type="text"
-                            value={this.state.title}
+                            value={this.props.data[0].volumeInfo.title}
+                            // value={this.state.title}
                             onChange={this.update("title")}
-                            placeholder="Add a Book..."
+                        />
+                        <br/>
+                        <input type="text"
+                            value={this.props.data[0].volumeInfo.categories[0]}
+                            // value={this.state.title}
+                            onChange={this.update("category")}
                         />
                         <input type="submit" value="Submit" />
                     </div>
--- frontend/src/components/search/search_book.jsx
+++ frontend/src/components/search/search_book.jsx
@@ -1,32 +1,28 @@
 import React, { useState, useEffect } from "react";
-import  ReactDOM  from "react-router-dom";
-import axios from 'axios';
-import  MetaTags  from 'react-meta-tags'
-
+import AddLeaf from '../leaves/add_leaf_container';
 
 
 export default function () {
     const [book, setBook] = useState("");
     const [result, setResult] = useState([]);
     const [apiKey, setApiKey] = useState("AIzaSyB8uY1e1Cxe0tLz_rRJtjqjOGZb3Sw2ITA");
-    useEffect(()=> {
-        
-        const script = document.createElement('script')
-        script.src = 'https://apis.google.com/js/platform.js'
-        script.async = true;
-        document.body.appendChild(script);
-
-        return () => {
-            document.body.removeChild(script);
-        }
-    }, []);
+    
     
     function handleSubmit(e) {
         e.preventDefault();
       
         fetch('https://www.googleapis.com/books/v1/volumes?q=' + book + '&key=' + apiKey)
             .then(response => response.json())
-            .then(data => setResult(data.items));
+            .then((data) => {
+                setResult(data.items);
+
+            });
+            if(!result) return null;
+            if(result.length === 0) return null;
+            console.log(result[0]);
+            console.log(result[0].volumeInfo.title);
+            console.log(result[0].volumeInfo.categorization[0]);
+        
     }
     function handleChange(e) {
         const book = e.target.value;
@@ -35,11 +31,6 @@ export default function () {
 
     return (
         <div>
-           
-            <MetaTags>
-                <meta name="google-signin-client_id" content="911098907499-qsvis5g9cis046r19e676hu1jlne0j3g.apps.googleusercontent.com"></meta>
-            </MetaTags>
-           
             <form onSubmit={handleSubmit}>
                 <input type="text"
                     onChange={handleChange}
@@ -47,6 +38,8 @@ export default function () {
                 />
                 <button type="submit">Search</button>
             </form>
+            
+            <AddLeaf data={result}/>
         </div>
     )
 }
