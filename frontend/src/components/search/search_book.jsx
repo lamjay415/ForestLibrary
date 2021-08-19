9e1caff6f8e5b3c0e90b4be0ae294e385bdc2f23
hilalbbalci <drhilalbiten@gmail.com>
2021-08-19 12:32:38 -0700

    get the data from google book api

--- frontend/src/components/search/search_book.jsx
+++ frontend/src/components/search/search_book.jsx
@@ -1,20 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import  ReactDOM  from "react-router-dom";
 import axios from 'axios';
-import { get } from "mongoose";
+import  MetaTags  from 'react-meta-tags'
+
 
 
 export default function () {
     const [book, setBook] = useState("");
     const [result, setResult] = useState([]);
-    const [apiKey, setApiKey] = useState("AIzaSyDQabk_IpaQPQbgX2a2Yes2QQiiUHxPC50");
+    const [apiKey, setApiKey] = useState("AIzaSyB8uY1e1Cxe0tLz_rRJtjqjOGZb3Sw2ITA");
+    useEffect(()=> {
+        
+        const script = document.createElement('script')
+        script.src = 'https://apis.google.com/js/platform.js'
+        script.async = true;
+        document.body.appendChild(script);
 
+        return () => {
+            document.body.removeChild(script);
+        }
+    }, []);
+    
     function handleSubmit(e) {
         e.preventDefault();
-        axios.get('https://www.googleapis.com/books/v1/volumes?q=' + book + '&key=' + apiKey)
-            .then(data=> {
-                setResult(data.data.items)
-            })
+      
+        fetch('https://www.googleapis.com/books/v1/volumes?q=' + book + '&key=' + apiKey)
+            .then(response => response.json())
+            .then(data => setResult(data.items));
     }
     function handleChange(e) {
         const book = e.target.value;
@@ -23,6 +35,11 @@ export default function () {
 
     return (
         <div>
+           
+            <MetaTags>
+                <meta name="google-signin-client_id" content="911098907499-qsvis5g9cis046r19e676hu1jlne0j3g.apps.googleusercontent.com"></meta>
+            </MetaTags>
+           
             <form onSubmit={handleSubmit}>
                 <input type="text"
                     onChange={handleChange}
