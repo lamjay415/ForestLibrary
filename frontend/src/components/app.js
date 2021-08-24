40c23655002ff61ce896fd9d2107639bc9e9c149
canberko560 <canberkvarli@gmail.com>
2021-08-24 11:54:50 -0700

    modify

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import {Route} from 'react-router-dom';
-// import { Switch } from 'react-router-dom';
+// import { Switch3 } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 import MainPage from './main/main_page';
 import LoginFormContainer from './session/login_form_container';
@@ -16,10 +16,12 @@ const App = () => (
         <NavBarContainer/>
         <div>
          <div>
-            <AuthRoute exact path="/login" component={LoginFormContainer} />
-            <AuthRoute exact path="/register" component={SignupFormContainer} />
-            <Route exact path="/users/:id" component={ProfileContainer} />
-            <Route exact path="/" component={ForestContainer}/>
+             {/* <Switch> */}
+                <AuthRoute exact path="/login" component={LoginFormContainer} />
+                <AuthRoute exact path="/register" component={SignupFormContainer} />
+                <Route exact path="/users/:id" component={ProfileContainer} />
+                <Route exact path="/" component={ForestContainer}/>
+             {/* </Switch> */}
             {/* <ProtectedRoute exact path="/users/userId" component={TreeCreateContainer} /> */}
         </div>
 \            {/* <ProtectedRoute exact path='/' component={LeavesContainer} /> */}
