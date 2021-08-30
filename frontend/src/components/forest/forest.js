8e339eff231adf487cf5115d6bfdfaa447c32852
canberko560 <canberkvarli@gmail.com>
2021-08-30 16:04:17 -0700

    configure main page

--- frontend/src/components/forest/forest.js
+++ frontend/src/components/forest/forest.js
@@ -39,37 +39,86 @@ export class Forest extends React.Component {
             () => this.setState({reload: false})
         )
     }
-    
+
+    existingForest = () => {
+        const { loggedIn, currentUser } = this.props;
+        const trees = (this.props.trees)
+    if(loggedIn) {
+        return(
+        <div>
+            <span className="welcome-user">Welcome home {currentUser}</span>
+            <div className="outer-forest">
+                <div className="forest">
+                    {trees.map((tree, idx) => (
+                        <div className="tree" key={idx}> 
+
+                    <Link className="links"
+                        to={`/users/${tree._id}`} >
+                            <img className="tree-images"
+                            src="https://image.flaticon.com/icons/png/64/4319/4319580.png" 
+                            onMouseEnter={() => this.setState({dataIsShown: true})}
+                            onMouseLeave={() => this.setState({dataIsShown: false})}
+                            /> 
+                        </Link> 
+                            <span className="tree-username">{tree.username}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
+        )}
+        else{
+            return(
+            <div>
+                <div className="advertisement">
+                    <span id="main-advertisement">What's next in reading is first on ForestLibrary</span>
+Upload your first leaf and begin your journey. ForestLibrary gives you space to create, find your book club, and connect with other readers.</div>
+             <div className="outer-forest">
+                <div className="forest">
+                    {trees.map((tree, idx) => (
+                        <div className="tree" key={idx}> 
+
+                    <Link className="links"
+                        to={`/users/${tree._id}`} >
+                            <img className="tree-images"
+                            src="https://image.flaticon.com/icons/png/64/4319/4319580.png" 
+                            onMouseEnter={() => this.setState({dataIsShown: true})}
+                            onMouseLeave={() => this.setState({dataIsShown: false})}
+                            /> 
+                        </Link> 
+                            <span className="tree-username">{tree.username}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <div className="bottom-ad">
+                <span className="bottom-ad" id="bottom-ad-title">Thanks for checking out the trees. Now join in.</span>
+                <span id="bottom-ad-desc">Save books, add reviews and build your tree. All for free.</span>
+                 <Link
+                        to="/register"
+                        className="create-tree-btn"
+                        id="upload-your-own-button"
+                    >
+                        Create a Tree</Link>
+            </div>
+             <Link
+                        to="/login"
+                        className="login-main-page"
+                        id="upload-your-own-button"
+                    >
+                        Log in</Link>
+        </div>
+            )
+        }
+    }
  
     render() {
         const trees = (this.props.trees)
         const {loggedIn} = this.props;
-
         if(!trees){
             return null
         }else{
-            return (
-        <div className="outer-forest">
-            
-            <div className="forest">
-                {trees.map((tree, idx) => (
-                    <div className="tree" key={idx}> 
-
-                   <Link className="links"
-                    to={`/users/${tree._id}`} >
-                        <img className="tree-images"
-                        src="https://image.flaticon.com/icons/png/64/4319/4319580.png" 
-                        onMouseEnter={() => this.setState({dataIsShown: true})}
-                        onMouseLeave={() => this.setState({dataIsShown: false})}
-                        /> 
-                    </Link> 
-                        <span className="tree-username">{tree.username}</span>
-                    </div>
-                
-                ))}
-            </div>
-        </div>
-        );
+            return this.existingForest();
         }
     }
 }
