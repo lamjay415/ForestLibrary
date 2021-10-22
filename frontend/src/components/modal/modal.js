3c45f4f21b36fd32136da17d6a31071d2044771c
canberko560 <canberkvarli@gmail.com>
2021-10-22 10:48:29 +0300

    add window click close func

--- frontend/src/components/modal/modal.js
+++ frontend/src/components/modal/modal.js
@@ -5,8 +5,17 @@ import './modal.css';
 const Modal = ({ handleClose, show, children }) => {
   const showHideClassName = show ? "modal display-block" : "modal display-none";
 
+  const handleWindowClose = (e) => {
+    if(e.target.className === "modal display-block"){
+      handleClose()
+    }
+    
+    console.log(e)
+  }
+  
+
   return (
-    <div className={showHideClassName}>
+    <div className={showHideClassName} onClick={handleWindowClose}>
       <section 
         className="modal-main">
         {children}
